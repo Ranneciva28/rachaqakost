82331086d03f3f94c696e21081c9fc405e8a3327
Ranneciva28 <reddingtongaming28@gmail.com>
2026-09-05 23:20:13 +0700

    Perbaiki tombol Lihat file KTP

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -9,7 +9,21 @@ class MediaController extends Controller
     public function show(MediaFile $media)
     {
         abort_unless(in_array($media->kind,['HERO','CATEGORY'],true),404);
-        return response($media->contents,200,['Content-Type'=>$media->mime_type,'Content-Length'=>(string)$media->size,
+        return $this->stream($media,['Content-Type'=>$media->mime_type,'Content-Length'=>(string)$media->size,
             'X-Content-Type-Options'=>'nosniff','Cache-Control'=>'public, max-age=604800, immutable']);
+    }
+
+    private function stream(MediaFile $media,array $headers)
+    {
+        $contents=$media->contents;
+        return response()->stream(function()use($contents){
+            if(is_resource($contents)){
+                $metadata=stream_get_meta_data($contents);
+                if($metadata['seekable']??false)rewind($contents);
+                fpassthru($contents);
+                return;
+            }
+            echo $contents;
+        },200,$headers);
     }
 }
