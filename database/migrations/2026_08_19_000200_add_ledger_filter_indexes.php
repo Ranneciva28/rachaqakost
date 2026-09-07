2202ed4a36a80ff868760468f7685a6ce3deb05a
Ranneciva28 <reddingtongaming28@gmail.com>
2026-09-07 08:46:29 +0700

    Make ledger index migration portable

--- database/migrations/2026_08_19_000200_add_ledger_filter_indexes.php
+++ database/migrations/2026_08_19_000200_add_ledger_filter_indexes.php
@@ -9,13 +9,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('payments', function (Blueprint $table) {
-            $table->dropIndex('payments_paid_at_idx');
             $table->index(['paid_at', 'id'], 'payments_paid_at_id_idx');
             $table->index('amount', 'payments_amount_idx');
         });
 
         Schema::table('expenses', function (Blueprint $table) {
-            $table->dropIndex('expenses_spent_at_idx');
             $table->index(['spent_at', 'id'], 'expenses_spent_at_id_idx');
             $table->index('amount', 'expenses_amount_idx');
         });
